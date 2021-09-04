5d25918db58f8ad118c24c2a8dab8de6e70303e1
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-09-04 11:54:27 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import {Options} from "./components/Options";
 import { AudioPlayer } from "./components/AudioPlayer";
 import { AudioRecorder } from "./components/AudioRecorder";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
+    
       <div className="app">
+        <Login />
         <Options />
-        
         <AudioPlayer/>
         <AudioRecorder />
       </div>
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <div className="loginContainer">
+            <input type="text" name="username" placeholder="username"/>
+            <input type="password" name="password" placeholder="password"/>
+            <button>Submit</button>
+        </div>
+    )
+}
